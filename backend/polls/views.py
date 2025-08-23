4fd89d2d505e94673c828e81c35e360741341fda
Jakub Kogut <jakub@jkogut.pl>
2025-08-23 20:16:12 +0200

    tutorial 4

--- backend/polls/views.py
+++ backend/polls/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.template import loader
 
+from .models import Question
 def index(request):
-    return HttpResponse("Hello, world! This is the index view of your Django application.")
+    latestQuestionList = Question.objects.order_by("-pub_date")[:5]
+    contex = {"latest_question_list": latestQuestionList}
+    return render(request, "polls/index.html", contex)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    print(
+    return render(request, "polls/detail.html", {"question": question})
+
+def results(request, question_id):
+    responce = "you are looking at the rsoults of qiestion %s."
+    return HttpResponse(responce % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
 
